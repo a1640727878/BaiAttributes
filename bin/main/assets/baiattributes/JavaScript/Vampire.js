4fe5a084555be5f75b27d72231ac3878f1068373
sky_bai <a1640727878@163.com>
2019-04-04 14:34:02 +0800

    清理一些没有用的东西

--- bin/main/assets/baiattributes/JavaScript/Vampire.js
+++
@@ -1,7 +0,0 @@
-PlayerAttributes = Java.type("sky_bai.sponge.BaiAttributes.PlayerAttributes");
-Keys = Java.type("org.spongepowered.api.data.key.Keys")
-function setDamage(damage,event)  {
-	entity = event.getSource().getSource();
-	if (entity instanceof Java.type("org.spongepowered.api.entity.living.player.Player")) {entity.offer(Keys.HEALTH, entity.health().get() + (damage * (new PlayerAttributes(entity).getItemAttributes().get("Vampire") / 100)))}
-	return damage
-}
